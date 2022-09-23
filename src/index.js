2fc70dd91e154d57c06ab5ad69fb6cb9d6c1e6d4
osvaldo <71652778+devOsvaldo2020@users.noreply.github.com>
2022-09-23 12:24:31 -0300

    tecnica pomodoro - paro a cada 30 minuto salvo, descando 5 e volto.

--- src/index.js
+++ src/index.js
@@ -15,17 +15,28 @@ const customers = [];
 app.post("/account", (request, response) => {
     const { cpf, name } = request.body;
 
-    const id = uuidv4();
+    const customerAlreadyExists = customers.some(
+        (customer) => customer.cpf === cpf
+    );
+    if (customerAlreadyExists) {
+        return response.status(400).json({ error: "Cliente já existe" }); //Customer Already exists
+    }
 
     customers.push({
         cpf,
         name,
-        id,
+        id: uuidv4(),
         statement: []
     });
     return response.status(201).send();
-});
+}); // create = criar -- account = conta
 
+app.get("/bankStatement/:cpf", (request, response) => {
+    const { cpf } = request.params;
+    // customer = cliente
+    const customer = customers.find(customer => customer.cpf === cpf);
+    return response.json(customer.statement)
+}); // search = buscar -- bank statement = extrato bancário |
 
 // portas
 const port = 3333
