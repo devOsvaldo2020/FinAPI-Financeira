5a974257028b56a2ead80124b1e521dbb81c5d07
osvaldo <71652778+devOsvaldo2020@users.noreply.github.com>
2022-09-23 11:29:26 -0300

    ok

--- src/index.js
+++ src/index.js
@@ -1,30 +1,34 @@
+const { response } = require('express');
 const express = require('express');
-
+const { v4: uuidv4 } = require("uuid");
 const app = express();
+app.use(express.json());
+
+const customers = [];
+// cpf - string
+// name - string 
+// id - uuid (install pelo terminal digite>> yarn add uuid)
+// statement []
+// 
 
 // rotas
-app.get("/courses", (req, res) => {
-    return res.json(["Curso 1", "Curso 2", "Curso 3"]);
+app.post("/account", (request, response) => {
+    const { cpf, name } = request.body;
+
+    const id = uuidv4();
+
+    customers.push({
+        cpf,
+        name,
+        id,
+        statement: []
+    });
+    return response.status(201).send();
 });
 
-app.post("/courses",(req, res)=> {
-    return res.json(["Curso 1","Curso 2","Curso 3", "Curso 4"]);
-});
-
-app.put("/courses/:id", (req, res) => {
-    return res.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"]);
-});
-
-app.patch("/courses/:id", (req, res) => {
-    return res.json(["Curso 6", "Curso 7", "Curso 3", "Curso 4"]);
-});
-
-app.delete("/courses/:id", (req, res) => {
-    return res.json(["Curso 6", "Curso 2", "Curso 4"]);
-});
 
 // portas
 const port = 3333
 app.listen(port);
 
-// localhost:3333/courses 
+// localhost:3333
