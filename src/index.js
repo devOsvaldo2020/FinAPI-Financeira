8a5d094f134809117d35edf7b32c097e337a579d
osvaldo <71652778+devOsvaldo2020@users.noreply.github.com>
2022-09-23 10:50:59 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+
+const app = express();
+
+// rotas
+app.get("/courses", (req, res) => {
+    return res.json(["Curso 1", "Curso 2", "Curso 3"]);
+});
+
+app.post("/courses",(req, res)=> {
+    return res.json(["Curso 1","Curso 2","Curso 3", "Curso 4"]);
+});
+
+app.put("/courses/:id", (req, res) => {
+    return res.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"]);
+});
+
+app.patch("/courses/:id", (req, res) => {
+    return res.json(["Curso 6", "Curso 7", "Curso 3", "Curso 4"]);
+});
+
+app.delete("/courses/:id", (req, res) => {
+    return res.json(["Curso 6", "Curso 2", "Curso 4"]);
+});
+
+// portas
+const port = 3333
+app.listen(port);
+
+// localhost:3333/courses 
